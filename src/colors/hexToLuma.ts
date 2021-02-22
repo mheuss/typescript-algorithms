116377715c218c9ef67dd5bb25d95afd7f1f17d5
Michael Heuss <mrheuss@gmail.com>
2021-02-22 08:05:55 -0500

    Added color shade and cleaned up tests and code around hex2luma

--- src/colors/hexToLuma.ts
+++ src/colors/hexToLuma.ts
@@ -13,9 +13,9 @@
 
 export function hexToLuma(color: string): number {
     const hex = color.replace(/#/, '')
-    const r = parseInt(hex.substr(0, 2), 16)
-    const g = parseInt(hex.substr(2, 2), 16)
-    const b = parseInt(hex.substr(4, 2), 16)
+    const red = parseInt(hex.substr(0, 2), 16)
+    const green = parseInt(hex.substr(2, 2), 16)
+    const blue = parseInt(hex.substr(4, 2), 16)
 
-    return [0.299 * r, 0.587 * g, 0.114 * b].reduce((a, b) => a + b) / 255
+    return [0.299 * red, 0.587 * green, 0.114 * blue].reduce((a, b) => a + b) / 255
 }
